4765c3cd34071ccb7bc5411cd0d2610bb302ff73
HAVibhavSharma <vibhavsharmaha@gmail.com>
2026-08-16 15:03:28 +0530

    fixed an import bug

--- vllm/v1/core/kv_cache_manager.py
+++ vllm/v1/core/kv_cache_manager.py
@@ -8,6 +8,7 @@ from typing import Any, Literal, overload
 
 from vllm.distributed.kv_events import KVCacheEvent
 from vllm.logger import init_logger
+from vllm.v1.core.hbm_summary import is_prefetch_only
 from vllm.v1.core.kv_cache_coordinator import get_kv_cache_coordinator
 from vllm.v1.core.kv_cache_metrics import KVCacheMetricsCollector
 from vllm.v1.core.kv_cache_utils import KVCacheBlock
